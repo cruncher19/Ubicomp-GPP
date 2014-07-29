62090d131cb1a7b1428d8ff6e89bf1b48e1d5119
Brad Covey <bradcovey@gmail.com>
2014-07-29 20:14:03 -0300

    modified the predictor to use forecasts instead of current weather readings

--- predictor.py
+++ predictor.py
@@ -3,41 +3,35 @@ from sklearn import linear_model
 from pyowm import OWM
 from utilities import getConfig
 
+# Accepts an array of weather data and the corresponding power production array
+# Creates a linear regression model from the data
+# returns an array containing predicted power production for the next three days
+def predict(weatherData, powerData):
+	# perform multivariate linear regression on the data to find the correlation between weather and power production
+	model = linear_model.LinearRegression()
+	model.fit(weatherData,powerData)
 
-# add code to pull data out of database through REST API when documentation on this is available
-powerData = None
-# Some fake placheolder data
-weatherData = array([[1,2,3,4,5],
-                    [5,4,3,2,1],
-                    [2,3,1,5,4],
-                    [3,4,1,2,5]])
+	# fetch the current forecast
+	config = getConfig('config.ini')
+	OWM_API_key = config.get('config', 'OWM_API_key').encode('ascii')
+	OWM_location = config.get('config', 'OWM_location').encode('ascii')
+	owm = OWM(OWM_API_key)
+	fc = owm.daily_forecast(OWM_location, limit=3)
+	f = fc.get_forecast()
 
-powerData = array([[10],
-                    [9],
-                    [8],
-                    [7]])
+	predictions = []
+	for w in f:
+		# Get all the variables we want to store in the database
+		cloudCover = w.get_clouds()
+		windSpeed = w.get_wind()['speed']
+		humidity = w.get_humidity()
+		pressure = w.get_pressure()['press']
+		temperature = w.get_temperature(unit='celsius')['temp']
+		status = w.get_status()
 
-# perform multivariate linear regression on the data to find the correlation between weather and power production
-model = linear_model.LinearRegression()
-model.fit(weatherData,powerData)
+		currentWeather = array([cloudCover, windSpeed, humidity, pressure, temperature])
 
-# fetch the current forecast
-config = getConfig('config.ini')
-OWM_API_key = config.get('config', 'OWM_API_key').encode('ascii')
-OWM_location = config.get('config', 'OWM_location').encode('ascii')
-owm = OWM(OWM_API_key)
-obs = owm.weather_at(OWM_location)
-w = obs.get_weather()
-
-# Get all the variables we want to store in the database
-cloudCover = w.get_clouds()
-windSpeed = w.get_wind()['speed']
-humidity = w.get_humidity()
-pressure = w.get_pressure()['press']
-temperature = w.get_temperature(unit='celsius')['temp']
-status = w.get_status()
-
-currentWeather = array([cloudCover, windSpeed, humidity, pressure, temperature])
-
-# use the regression model to generate production predictions based on the current forecast
-print "Predicted power production: " + str(model.predict(currentWeather))
+		# use the regression model to generate production predictions based on the current forecast
+		# print "Predicted power production: " + str(model.predict(currentWeather))
+		predictions.append(model.predict(currentWeather))
+	return predictions
