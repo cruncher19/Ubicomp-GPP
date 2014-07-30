f14c4fdbb8411b12a237dd9979afe97d1a0e9513
Brad Covey <bradcovey@gmail.com>
2014-07-30 20:38:25 -0300

    Added the code to pull power production from the arduino to scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,17 +2,29 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from time import sleep
 import logging
 import predictor
+from utilities import getConfig
+import requests
+
+config       = getConfig('config.ini')
+arduinoAddress  = config.get('config', 'arduino_address').encode('ascii')
 
 logging.basicConfig()
 
 def update():
 	predictor.predict()
 
+def pullPowerProduction():
+	# powerProduction = Get the reading from the arduino when the API call is implemented
+	# requests.get("http://localhost:5000/storePowerProduction?powerLevel=" + powerProduction)
+	print "pulled power production data"
+
+
 scheduler = BackgroundScheduler()
 
 scheduler.start()
 
-scheduler.add_job(update, 'interval', minutes=1)
+scheduler.add_job(update, 'interval', hours=1)
+scheduler.add_job(pullPowerProduction, 'interval', minutes=30)
 
 while True:
 	sleep(1)
